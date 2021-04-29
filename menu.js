b00dedd04acfc5c1795967f66779ae0511c5d56e
junkmaker <ksk_h12@yahoo.co.jp>
2021-04-29 13:15:13 +0900

    メニュークリックでノートを変更する

--- menu.js
+++ menu.js
@@ -1,12 +1,19 @@
 console.log("welcome");
 
+// 要素を上書き
+function OnLinkClick(id) {
+    console.log(`note${id}.html`);
+    document.getElementById('id_note').src=`note${id}.html`;    
+}
+
+
 // REST API
-fetch('https://zipcloud.ibsnet.co.jp/api/search?zipcode=2200011')
-.then(response => {
-    console.log(response);
-    return response.json();
-}).then(res => {
-    console.log(res.items[0].volumeInfo.tetle);
-}).catch(error => {
-    console.log(error);
-});
+// fetch('https://zipcloud.ibsnet.co.jp/api/search?zipcode=2200011')
+// .then(response => {
+//     console.log(response);
+//     return response.json();
+// }).then(res => {
+//     console.log(res.items[0].volumeInfo.tetle);
+// }).catch(error => {
+//     console.log(error);
+// });
